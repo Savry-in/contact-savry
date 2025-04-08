93d069d3e538e31468ef73fcddf3b45d78a1621a
Utkarsh Dwivedi <utkrsh.dev@gmail.com>
2025-04-08 16:23:23 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id='main' className="bg-[#E0FAEE] min-h-screen">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
